40aac29683407971b101239274a98318aa205d88
Aleksandr <ikonnikovAI@lad24.ru>
2025-02-08 15:13:27 +0600

    chore: add static string translation and some utils in i18n dir

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+  ru: "Русский",
+  en: "English",
+};
+
+export const defaultLang = "ru";
+
+export const ui = {
+  ru: {
+    "nav.about": "Обо мне",
+    "nav.stack": "Технологии",
+    "nav.experience": "Опыт",
+    "nav.contacts": "Контакты",
+
+    "contact me": "Связаться со мной",
+  },
+  en: {
+    "nav.about": "About",
+    "nav.stack": "Stack",
+    "nav.experience": "Experience",
+    "nav.contacts": "Contacts",
+
+    "contact me": "Contact me",
+  },
+} as const;
