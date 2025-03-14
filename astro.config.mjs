af4f5a084583f20336ace0f1b2520de274312c7c
Aleksandr <ikonnikovAI@lad24.ru>
2025-03-14 19:41:41 +0600

    fix: rollback

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-import preact from "@astrojs/preact";
-
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-  },
-  integrations: [preact()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
